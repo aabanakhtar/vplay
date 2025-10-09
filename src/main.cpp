2b066d3bcdefae3dede631fd0ef969a0ec3682a1
aabanakhtar <aabanakhtar.home@gmail.com>
2025-10-09 19:28:27 -0400

    god fricking damn linker errors

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,27 @@
 // Created by aabanakhtar on 9/29/25.
 //
 #include <iostream>
+extern "C" {
+    #include <libavcodec/avcodec.h>
+    #include <libavformat/avformat.h>
+}
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2) 
+    std::string filename = "vid.mp4";
+    AVFormatContext* video_fmt_ctx = avformat_alloc_context();
+    if (video_fmt_ctx == nullptr)
     {
-        std::cerr << "Usage: " << argv[0] << " <project_name>" << std::endl;
+        std::cerr << "Could not allocate context!" << std::endl;
     }
 
-    std::string project_name = "";
-    std::cout << "Loading Project: " << project_name << std::endl;
+    if (avformat_open_input(&video_fmt_ctx, filename.c_str(), nullptr, nullptr) != 0) 
+    {
+        std::cerr << "Could not open the file!" << std::endl;
+    }
+
+    std::cout << "Video format: " << video_fmt_ctx->iformat->name << ", Duration: " << video_fmt_ctx->duration << ", and cool!\n";
+    
+    avformat_free_context(video_fmt_ctx);
     return 0;
 }
